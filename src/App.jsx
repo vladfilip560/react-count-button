a82fc8ffb30ac02d59956d50ce0e1a95272c62a5
vladfilip560 <117026688+vladfilip560@users.noreply.github.com>
2025-03-18 14:28:07 +0200

    now the number is prettier
    css adition to the program. The number is now PINK!

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Button from "./Button"
 import Count from "./Count"
 import {useState} from 'react'
+import './App.css'
 function App() {
   let [number, setNumber] = useState(0);
   return(
-  <>
+  <div className='app'>
     <Count number={number}/>
     <Button setNumber={setNumber}/>
-  </>
+  </div>
   );
 }
 
--- src/Count.jsx
+++ src/Count.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import './App.css'
 export default function Count({number}) {
   return (
-    <span>{number}</span>
+    <span className='count'>{number}</span>
   );
 }
